2c8a24b98c091a23dece91542c38dbe403734a59
Michael Robinson <michael.robinson@westernsouthernlife.com>
2018-03-16 12:06:45 -0400

    added function

--- student.class.ts
+++ student.class.ts
@@ -8,4 +8,10 @@ export class Student {
   Year: string;
   Active: boolean; // added this one
   DateCreated: string;
+
+  robinson(id: number, firstname: string, lastname: string) {
+		
+		console.log("Id: ", this.Id, "Firstname: ", this.Firstname, "Lastname: ", this.Lastname);
+
+	}
 }
