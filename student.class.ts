2ae7e668252c738081fd98359c82c72e0f2809e6
Nate Bucher <n8bizall@yahoo.com>
2018-03-16 12:04:26 -0400

    added console.log msg

--- student.class.ts
+++ student.class.ts
@@ -8,5 +8,10 @@ export class Student {
   Year: string;
   Active: boolean; // added this one
   DateCreated: string;
-  constructor(){}
-}
+  constructor(FirstName: string){
+    function Bucher(){
+      console.log("My student name is" + FirstName + " .")
+  }
+
+
+}}
