af27384f383dd4495cfd74421f6563da258c7989
cfox38 <christina.fox@westernsouthernlife.com>
2018-03-16 12:32:38 -0400

    lastname

--- student.class.ts
+++ student.class.ts
@@ -9,5 +9,28 @@ export class Student{
 	Active: boolean; //added this one
 	DateCreated: string; 
 
-	constructor() {}
+	constructor(id: number, firstname: string, lastname: string, gpa: number, sat: number, major: string, year: string, active: boolean){
+		this.Id = id;
+		this.Firstname = firstname;
+		this.Lastname = lastname;
+		this.GPA = gpa;
+		this.SAT= sat;
+		this.Major= major;
+		this.Year= year;
+		this.Active = active;
+
+	}
+
+	printOut(): void {
+		console.log(`id=${this.Id}, firstname=${this.Firstsname},lastname=${this.Lastname},gpa=${this.GPA}, sat=${this.SAT}, major=${this.Major},
+		 year=${this.Year}, active=${this.Active}`);
+	}
+}
+
+let custs: Student[] = [
+	new Student(1, "Christina", "Fox", 3.9, 1240, "Business", "Senior", True,)
+	];
+
+for(let student of stud) {
+	student.printOut();
 }
