6a70929d7d58dbdf98553994771b97b1f3b8fc5e
cfox38 <christina.fox@westernsouthernlife.com>
2018-03-16 11:42:00 -0400

    date created

--- student.class.ts
+++ student.class.ts
@@ -1,10 +1,13 @@
-export class Student {
-  Id: number;
-  Firstname: string;
-  Lastname: string;
-  GPA: number;
-  SAT: number;
-  Major: string;
-  Year: string;
-  Active: boolean; // added this one
+export class Student{
+	Id: number;
+	Firstname: string;
+	Lastname: string; 
+	GPA: number;
+	SAT: number; //added this 
+	Major: string;
+	Year: string;
+	Active: boolean; //added this one
+	DateCreated: string; 
+
+	constructor() {}
 }
